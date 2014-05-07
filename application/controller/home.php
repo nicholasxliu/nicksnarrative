3fd5053a71e8a97f088c9b9ee15b5dbf0c4fb0aa
nicholasxliu <nicholas.x.liu@gmail.com>
2014-05-07 01:00:51 -0400

    eliminate homepage header

--- application/controller/home.php
+++ application/controller/home.php
@@ -11,8 +11,6 @@ class Home extends Controller
      */
     public function index()
     {
-        require 'application/views/templates/header.php';
         require 'application/views/home/index.php';
-        require 'application/views/templates/footer.php';
     }
 }
