92c1f0b026cb60e4b71b854a9d736750ce2563cf
nicholasxliu <nicholas.x.liu@gmail.com>
2014-05-07 00:19:55 -0400

    update url

--- application/config/config.php
+++ application/config/config.php
@@ -18,7 +18,7 @@ ini_set("display_errors", 1);
  * Configuration for: Project URL
  * Put your URL here, for local development "127.0.0.1" or "localhost" (plus sub-folder) is fine
  */
-define('URL', 'http://grantfellow.com/');
+define('URL', 'http://nicksnarrative.com/');
 
 /**
  * Configuration for: Database
