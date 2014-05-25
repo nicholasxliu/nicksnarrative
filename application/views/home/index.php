7553e4d4e3f71e94e3892c3363c7aae47408b6ae
nicholasxliu <nicholas.x.liu@gmail.com>
2014-05-25 19:23:09 -0400

    loginbugtesting

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -156,7 +156,7 @@ form.signin input::-webkit-input-placeholder { color:#bbb; text-shadow:0 0 2px #
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
-	$("a.(btn btn-default btn-lg)").click(function() {
+	$("a.btn btn-default)").click(function() {
 		
 		// Getting the variable's value from a link 
 		var loginBox = $(this).attr('href');
@@ -199,7 +199,7 @@ $(document).ready(function() {
 		<div class="post">
     	<h2>Your Login or Sign In Box</h2>
         	<div class="btn-sign">
-				<a href="#login-box" class="btn btn-default btn-lg">Login / Sign In</a>
+				<a href="#login-box" class="btn btn-default">Login / Sign In</a>
         	</div>
 		</div>
         
