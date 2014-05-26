279d25154b8196155c9d5b3e443f62b92b6011f8
nicholasxliu <nicholas.x.liu@gmail.com>
2014-05-25 21:45:40 -0400

    loginbugtest

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -21,7 +21,7 @@
 
 <script type="text/javascript">
 $(document).ready(function() {
-    $('.btn,.btn-login,.btn-default').click(function() {
+    $('.btn,.btn-default,.btn-lg').click(function() {
         
         // Getting the variable's value from a link 
         var loginBox = $(this).attr('href');
@@ -136,33 +136,32 @@ $(document).ready(function() {
                     <p>For the time being, this remains my personal journal. Please let me know if you would like access.</p>
                     <a href="#login-box" class="btn btn-default btn-lg">Login</a>
                 </div>
-                <div id="login-box" class="login-popup">
-                    <a href="#" class="close"><img src="close_pop.png" class="btn_close" title="Close Window" alt="Close" /></a>
-                    <form method="post" class="signin" action="#">
-                        <fieldset class="textbox">
-                            <label class="username">
-                                <span>Username or email</span>
-                                <input id="username" name="username" value="" type="text" autocomplete="on" placeholder="Username">
-                            </label>
-
-                            <label class="password">
-                                <span>Password</span>
-                                <input id="password" name="password" value="" type="password" placeholder="Password">
-                            </label>
-
-                            <button class="submit button" type="button">Sign in</button>
-
-                            <p>
-                                <a class="forgot" href="#">Forgot your password?</a>
-                            </p>
-
-                        </fieldset>
-                    </form>
-                </div>
             </div>
         </div>
 
-        
+        <div id="login-box" class="login-popup">
+            <a href="#" class="close"><img src="close_pop.png" class="btn_close" title="Close Window" alt="Close" /></a>
+            <form method="post" class="signin" action="#">
+                <fieldset class="textbox">
+                    <label class="username">
+                        <span>Username or email</span>
+                        <input id="username" name="username" value="" type="text" autocomplete="on" placeholder="Username">
+                    </label>
+
+                    <label class="password">
+                        <span>Password</span>
+                        <input id="password" name="password" value="" type="password" placeholder="Password">
+                    </label>
+
+                    <button class="submit button" type="button">Sign in</button>
+
+                    <p>
+                        <a class="forgot" href="#">Forgot your password?</a>
+                    </p>
+
+                </fieldset>
+            </form>
+        </div>
 
     </section>
 
